57276f26ff47ec11b9449138a28d52cf785e8701
V5796 (Course ID) <v5796@cam.ac.uk>
2015-08-14 11:02:59 +0100

    Finished module One. shorter move times altitude awareness

--- drone-backend.js
+++ drone-backend.js
@@ -11,26 +11,40 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav",{
+        driver:"ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
     
 // Fly the bot
 function fly(robot) {
-    robot = bot;
+    bot = robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
+    bot.nav.on("navdata", function(data){
+        console.log(data);
+    });
     bot.drone.disableEmergency();
     bot.drone.ftrim();
     bot.drone.takeoff();
+    bot.nav.on("altitudeChange", function(){
+         console.log("Altitude:", data);
+        if (altitude > 1.5 ){
+            bot.drone.land();
+        }
+    });
     after(10*1000,function(){
         bot.drone.forward(0.1);
         bot.drone.left(0.1);
     });
-    after(20*1000, function(){
+    after(12*1000, function(){
         bot.drone.back(0.1);
         bot.drone.right(0.1);
     });
-    after(25*1000, function(){
+    after(14*1000, function(){
         bot.drone.land();
     });
-    after(30*1000, function(){
+    after(19*1000, function(){
         bot.drone.stop();
     });
 
